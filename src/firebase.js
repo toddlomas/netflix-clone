19adee0ae5329ec826a873bd50f7ad91f8f8a05b
Todd Lomas <todd.lomas@hotmail.co.uk>
2023-01-17 19:22:55 +0000

    First day of Netflix Build

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDveB64lyZq8Id5b_NEhuirRxmIR2mhTmc",
+  authDomain: "netflix-clone-b36bd.firebaseapp.com",
+  projectId: "netflix-clone-b36bd",
+  storageBucket: "netflix-clone-b36bd.appspot.com",
+  messagingSenderId: "530896098592",
+  appId: "1:530896098592:web:2b682ee2172927cf7ff345",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
